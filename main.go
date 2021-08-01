74f9822635552ea02d78023e906c23fda6c50b39
Sameera Perera <sameera.perera@safetyculture.io>
2021-08-02 09:17:33 +1000

    new main implementation using worker nodes

--- main.go
+++ main.go
@@ -1,9 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"github.com/sap4001/nearmap-go/datasource"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 func main() {
+	db := getPopulatedDB()
+	dc := datasource.NewDistributedCache()
+	ds := datasource.NewDataStore(db, dc)
+
+	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
-		go func() {
-			// TODO:
-		}()
+		wg.Add(1)
+		go makeRandomRequests(&wg, ds, i+1, 50)
 	}
+	fmt.Println("Main: Waiting for workers to finish")
+	wg.Wait()
+	fmt.Println("Main: Completed")
+}
+
+func getPopulatedDB() datasource.Database {
+	db := datasource.NewDatabase()
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("key%d", i)
+		value := fmt.Sprintf("value%d", i)
+		db.Store(key, value)
+	}
+	return db
+}
+
+func makeRandomRequests(wg *sync.WaitGroup, ds datasource.DataStore, id, count int) {
+	fmt.Printf("Worker %v: Started\n", id)
+	defer wg.Done()
+	for i := 0; i < 50; i++ {
+		randKey := getRandKey()
+		start := time.Now()
+		ds.Value(randKey)
+		elapsed := time.Since(start)
+		fmt.Printf("RoutineID:%d [%d] Request %q time: %s\n", id, i, randKey, elapsed)
+	}
+	fmt.Printf("Worker %v: Finished\n", id)
+}
+
+func getRandKey() string {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 11
+	randI := rand.Intn(max-min) + min
+	//randI = 10//// //TODO
+	return fmt.Sprintf("key%d", randI)
 }
